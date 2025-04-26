9c23b2a64c0daaed4d883abd977ede0fa94842d2
Mohamedan Farag <120517880+Mohamedan-Farag@users.noreply.github.com>
2025-04-27 02:30:38 +0300

    Add files via upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { IncomesModule } from './finance/incomes/incomes.module';
+
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, IncomesModule],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class AppModule {}
